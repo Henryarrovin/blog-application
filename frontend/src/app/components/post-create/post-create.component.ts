e451f28bfc21b85ed98f57cbdec007d9942f45e8
Henry <henry.a.s.arrovin@gmail.com>
2024-09-29 17:57:08 +0530

    Updated post create component.

--- frontend/src/app/components/post-create/post-create.component.ts
+++ frontend/src/app/components/post-create/post-create.component.ts
@@ -1,24 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from '../../services/blog-service.service';
-import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post-create',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule ,FormsModule, RouterModule, ReactiveFormsModule],
   templateUrl: './post-create.component.html',
   styleUrl: './post-create.component.css'
 })
-export class PostCreateComponent {
-  post: any = { title: '', content: '', author: '' };
+export class PostCreateComponent implements OnInit{
+  postForm: FormGroup;
 
-  constructor(private blogService: BlogService, private router: Router) {}
+  constructor(private blogService: BlogService, private router: Router, private fb: FormBuilder) {
+    this.postForm = this.fb.group({
+      title: ['', Validators.required],
+      content: ['', Validators.required],
+      author: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    this.postForm = this.fb.group({
+      title: ['', Validators.required],
+      content: ['', Validators.required],
+      author: ['', Validators.required]
+    });
+  }
+
+  get titleControl() {
+    return this.postForm.get('title')!;
+  }
+
+  get contentControl() {
+    return this.postForm.get('content')!;
+  }
+
+  get authorControl() {
+    return this.postForm.get('author')!;
+  }
 
   createPost(): void {
-    this.blogService.createPost(this.post).subscribe(() => {
-      this.router.navigate(['/posts']);
-    });
+    if (this.postForm.valid) {
+      const post = this.postForm.value;
+      this.blogService.createPost(post).subscribe(() => {
+        this.router.navigate(['/posts']);
+      });
+    } else {
+      alert('Please fill in all required fields');
+    }
   }
 
 }
--- frontend/src/app/components/post-detail/post-detail.component.ts
+++ frontend/src/app/components/post-detail/post-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from '../../services/blog-service.service';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-post-detail',
@@ -12,13 +12,23 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
 export class PostDetailComponent implements OnInit {
   post: any;
 
-  constructor(private blogService: BlogService, private route: ActivatedRoute) {}
+  constructor(private blogService: BlogService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.blogService.getPostById(id!).subscribe(data => {
       this.post = data;
     });
+  }
+
+  deletePost(): void {
+    if (confirm('Are you sure you want to delete this post?')) {
+      const id = this.route.snapshot.paramMap.get('id');
+      this.blogService.deletePost(id!).subscribe(() => {
+        alert('Post deleted successfully');
+        this.router.navigate(['/posts']);
+      });
+    }
   }
 
 }
