3ac8170caadc7cd61e459bf369c87582a9f60cb9
Henry <henry.a.s.arrovin@gmail.com>
2024-09-29 16:37:58 +0530

    Created app routes.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { PostCreateComponent } from './components/post-create/post-create.component';
+import { PostUpdateComponent } from './components/post-update/post-update.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: PostListComponent },
+    { path: 'posts', component: PostListComponent },
+    { path: 'post/:id', component: PostDetailComponent },
+    { path: 'create', component: PostCreateComponent },
+    { path: 'update/:id', component: PostUpdateComponent },
+    { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
